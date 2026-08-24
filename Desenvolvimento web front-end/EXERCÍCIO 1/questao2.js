6b12eab4efb0fffd0003288a949ffc573b52e467
Vivian Varela <viviantvarela@gmail.com>
2026-08-24 17:16:50 -0300

    Fix calculation and output for final grade requirement

--- Desenvolvimento web front-end/EXERCÍCIO 1/questao2.js
+++ Desenvolvimento web front-end/EXERCÍCIO 1/questao2.js
@@ -4,8 +4,8 @@ const notaAVfinal = 120 - mediaParcial;
 if (notaAVfinal > 100) {
   console.log(`Com a média parcial de ${mediaParcial}, você precisaria de ${notaAVfinal} na avaliação final. Como a nota máxima é 100, não é possível atingir a média.`);
 }
+  
 else {
   const mediaFinal = (mediaParcial + notaAVfinal) / 2;
-  
   console.log(`A nota que você precisa na avaliação final é ${notaAVfinal}, com isso sua média final será ${mediaFinal}!`);
 }  
