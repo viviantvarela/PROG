63fea9de8a92a5d1880e3f06d3dcd08adb65915b
Vivian Varela <viviantvarela@gmail.com>
2026-08-24 16:56:21 -0300

    Refactor final grade calculation and output logic
    Fix calculation and output for final grade requirement.

--- Desenvolvimento web front-end/EXERCÍCIO 1/questao2.js
+++ Desenvolvimento web front-end/EXERCÍCIO 1/questao2.js
@@ -2,6 +2,12 @@ const mediaParcial = 48;
 const mediaIFRN = 60;
 
 const notaAVfinal = (mediaIFRN * 2) - mediaParcial;
-const mediaFinal = (mediaParcial + notaAVfinal) / 2;
 
-console.log(`A nota que você precisa na avaliação final é ${notaAVfinal}, com isso sua média final será ${mediaFinal}!`);
+if (notaAVfinal > 100) {
+  console.log(`Com a média parcial de ${mediaParcial}, você precisaria de ${notaAVfinal} na avaliação final. Como a nota máxima é 100, não é possível atingir a média.`);
+}
+else {
+  const mediaFinal = (mediaParcial + notaAVfinal) / 2;
+  
+  console.log(`A nota que você precisa na avaliação final é ${notaAVfinal}, com isso sua média final será ${mediaFinal}!);
+}  
