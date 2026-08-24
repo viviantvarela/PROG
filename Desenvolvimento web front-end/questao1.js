75d52a361dc5780980d314325f6b76c3cb653bea
Vivian Varela <viviantvarela@gmail.com>
2026-08-24 16:19:32 -0300

    Fix variable names for volume calculation

--- Desenvolvimento web front-end/questao1.js
+++ Desenvolvimento web front-end/questao1.js
@@ -1,7 +1,7 @@
 const raio = 5;
 const altura = 10;
 
-const volume = Math.PI * (r ** 2) * h;
+const volume = Math.PI * (raio ** 2) * altura;
 
 console.log(`Raio: ${raio} | Altura: ${altura}`);
 console.log(`Volume do cilindro: ${volume.toFixed(2)}`);
