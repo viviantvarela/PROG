94c0c6be2224c00d7ef961c97bf4851a0e784f7b
Vivian Varela <viviantvarela@gmail.com>
2026-08-24 17:11:20 -0300

    Fix IRA value type from string to number

--- Desenvolvimento web front-end/EXERCÍCIO 1/questao4.js
+++ Desenvolvimento web front-end/EXERCÍCIO 1/questao4.js
@@ -2,7 +2,7 @@ let aluno = {
    "nome" : "Maria",
    "matricula" : "202092923222",
    "curso" : "Informática",
-   "ira" : "83.2"
+   "ira" : 83.2
 };
 
 console.log(`${aluno.nome} é aluna do curso de ${aluno.curso} com matrícula ${aluno.matricula} e possui IRA ${aluno.ira}.`);
