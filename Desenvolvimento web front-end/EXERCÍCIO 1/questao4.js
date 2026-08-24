c705316b570f072e1aec3d34359bf1b10b8cf7fb
Vivian Varela <viviantvarela@gmail.com>
2026-08-24 17:29:06 -0300

    Update student details in questao4.js

--- Desenvolvimento web front-end/EXERCÍCIO 1/questao4.js
+++ Desenvolvimento web front-end/EXERCÍCIO 1/questao4.js
@@ -1,8 +1,8 @@
 let aluno = {
-   "nome" : "Maria",
-   "matricula" : "202092923222",
-   "curso" : "Informática",
-   "ira" : 83.2
+   "nome" : "Vivian",
+   "matricula" : "123456789101",
+   "curso" : "TSI",
+   "ira" : 84.31
 };
 
 console.log(`${aluno.nome} é aluna do curso de ${aluno.curso} com matrícula ${aluno.matricula} e possui IRA ${aluno.ira}.`);
