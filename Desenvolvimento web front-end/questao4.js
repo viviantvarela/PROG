e59a05dcba0b134474e23df127f11d22a9d981cb
Vivian Varela <viviantvarela@gmail.com>
2026-08-24 16:00:40 -0300

    Add aluno object and log student information

---
+++ Desenvolvimento web front-end/questao4.js
@@ -0,0 +1,8 @@
+let aluno = {
+   "nome" : "Maria",
+   "matricula" : "202092923222",
+   "curso" : "Informática",
+   "ira" : "83.2"
+};
+
+console.log(`${aluno.nome} é aluna do curso de ${aluno.curso} com matrícula ${aluno.matricula} e possui IRA ${aluno.ira}.`);
