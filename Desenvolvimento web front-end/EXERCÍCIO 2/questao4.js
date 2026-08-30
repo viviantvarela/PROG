a7076852f31ce2bc52724a510d13a333e5ec0078
Vivian Varela <viviantvarela@gmail.com>
2026-08-30 14:46:34 -0300

    Mudanças na questão 4

--- Desenvolvimento web front-end/EXERCÍCIO 2/questao4.js
+++ Desenvolvimento web front-end/EXERCÍCIO 2/questao4.js
@@ -1,12 +1,13 @@
 // Questão 4
 
-let altura = number(prompt(`Digite sua altura em centimetros:`));
-let sexo = prompt(`Digite 'm' para masculino ou 'f' para feminino:`).toLowerCase();
+let altura = Number(prompt(`Digite sua altura em centimetros: `));
+let sexo = prompt(`Digite 'm' para masculino ou 'f' para feminino: `).toLowerCase();
+let peso;
 
 if (sexo == 'm'){
-  peso = altura - 100 - (altura - 150 / 4)
+  peso = (altura - 100) - ((altura - 150) / 4);
 } else {
-  peso = altura - 100 - (altura - 150 / 2)
+  peso = (altura - 100) - ((altura - 150) / 2);
 }
    
-console.log(`A altura é ${altura}, o sexo é ${sexo}`)
+console.log(`A sua altura em centímetros é ${altura}cm, seu sexo é ${sexo}, e o seu peso ideal é ${peso.toFixed(2)}kg.`)
