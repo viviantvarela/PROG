eae2eb979996eb69cdb7c0edbe25a4f5b95ee4ba
Vivian Varela <viviantvarela@gmail.com>
2026-08-31 16:28:16 -0300

    Ajustes
    Refactor console.log statements for clarity.

--- Desenvolvimento web front-end/EXERCÍCIO 2/questao1.js
+++ Desenvolvimento web front-end/EXERCÍCIO 2/questao1.js
@@ -4,8 +4,9 @@ let a = 4;
 let b = 9;
 let c = a + b;
 
-console.log(`O valor da variável "a" é ${a}, o da variável "b" é ${b}`)
-console.log(`e o da "c" é: a + b = ${c}!`)
+console.log(`O valor da variável "a" é ${a},`)
+console.log(`da variável "b" é ${b}`)
+console.log(`e o da "c" é: (a + b) = ${c}!`)
 
 // Questão 1 (B)
 
