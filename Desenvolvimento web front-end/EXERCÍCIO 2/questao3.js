c18b1df750481c482515cc95d34543d75d932cfe
Vivian Varela <viviantvarela@gmail.com>
2026-08-31 16:39:59 -0300

    Mudanças

--- Desenvolvimento web front-end/EXERCÍCIO 2/questao3.js
+++ Desenvolvimento web front-end/EXERCÍCIO 2/questao3.js
@@ -8,7 +8,7 @@ const mediaParcial = (bimestre1 * 2 + bimestre2 * 3) / 5;
 if (mediaParcial >= 60.00) {
     console.log(`A média parcial do aluno é ${mediaParcial.toFixed(2)}`)
     console.log(`Aprovado!`);
-} else if (mediaParcial < 60 && mediaParcial >= 10) {
+} else if ( mediaParcial >= 10) {
     console.log(`A média parcial do aluno é ${mediaParcial.toFixed(2)}`)
     console.log(`Prova final!`);
 } else {
