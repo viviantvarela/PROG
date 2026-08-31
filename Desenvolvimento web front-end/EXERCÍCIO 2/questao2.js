d71cdb61f522af64390eb9eac38f50974521edff
Vivian Varela <viviantvarela@gmail.com>
2026-08-31 16:09:27 -0300

    Atualização da questão
    Refactor triangle validation logic into a boolean variable and log the result.

--- Desenvolvimento web front-end/EXERCÍCIO 2/questao2.js
+++ Desenvolvimento web front-end/EXERCÍCIO 2/questao2.js
@@ -4,10 +4,5 @@ let ladoA = 5;
 let ladoB = 8;
 let ladoC = 10;
 
-if (ladoA < (ladoB + ladoC) && ladoB < (ladoA + ladoC) && ladoC < (ladoA + ladoB)){
-  console.log(`A medida A é ${ladoA}, a medida B é ${ladoB} e a medida C é ${ladoC}. Essas medidas PODEM formar um triângulo, porque os seus três lados são menores que a soma dos outros dois.`);
-} else {
-  console.log(`A medida A é ${ladoA}, a medida B é ${ladoB} e a medida C é ${ladoC}. Essas medidas NÃO PODEM formar um triângulo, pois pelo menos um dos lados é maior ou igual a soma dos outros dois.`);
-}
-
-
+let formarTriangulo = ladoA < (ladoB + ladoC) && ladoB < (ladoA + ladoC) && ladoC < (ladoA + ladoB);
+console.log(formarTriangulo);
