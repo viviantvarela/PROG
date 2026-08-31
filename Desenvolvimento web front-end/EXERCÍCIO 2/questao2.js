d6bb6be4378eec201f2cfd9db290f7d228d34cbc
Vivian Varela <viviantvarela@gmail.com>
2026-08-31 17:36:23 -0300

    Alterações

--- Desenvolvimento web front-end/EXERCÍCIO 2/questao2.js
+++ Desenvolvimento web front-end/EXERCÍCIO 2/questao2.js
@@ -4,5 +4,5 @@ let ladoA = parseInt(prompt(`Digite um número inteiro para o lado A: `));
 let ladoB = parseInt(prompt(`Digite um número inteiro para o lado B: `));
 let ladoC =  parseInt(prompt(`Digite um número inteiro para o lado C: `));
 
-let formarTriangulo = ladoA < (ladoB + ladoC) && ladoB < (ladoA + ladoC) && ladoC < (ladoA + ladoB);
+const formarTriangulo = ladoA < (ladoB + ladoC) && ladoB < (ladoA + ladoC) && ladoC < (ladoA + ladoB);
 console.log(formarTriangulo);
