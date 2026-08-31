eefb8751a198c29135a3ee5a59d2020cdb4d074b
Vivian Varela <viviantvarela@gmail.com>
2026-08-31 16:56:42 -0300

    Alterações

--- Desenvolvimento web front-end/EXERCÍCIO 2/questao4.js
+++ Desenvolvimento web front-end/EXERCÍCIO 2/questao4.js
@@ -2,12 +2,14 @@
 
 let altura = Number(prompt(`Digite sua altura em centimetros: `));
 let sexo = prompt(`Digite 'm' para masculino ou 'f' para feminino: `).toLowerCase();
-let pesoIdeal;
+let k;
 
 if (sexo == 'm') {
-    pesoIdeal = (altura - 100) - ((altura - 150) / 4);
+    k = 4;
 } else {
-    pesoIdeal = (altura - 100) - ((altura - 150) / 2);
+    k = 2;
 }
+
+let pesoIdeal = (altura - 100) - ((altura - 150) / k);
 
 console.log(`A sua altura em centímetros é ${altura}cm, seu sexo é ${(sexo == `f`) ? `feminino` :  `masculino`}, e o seu peso ideal é ${pesoIdeal.toFixed(2)}kg.`)
