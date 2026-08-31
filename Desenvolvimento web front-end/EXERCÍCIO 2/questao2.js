a92a295042d0ded6f4f25627f768b507b9ec2e39
Vivian Varela <viviantvarela@gmail.com>
2026-08-31 16:35:01 -0300

    Atualização da questão
    Updated variables to accept user input for triangle sides.

--- Desenvolvimento web front-end/EXERCÍCIO 2/questao2.js
+++ Desenvolvimento web front-end/EXERCÍCIO 2/questao2.js
@@ -1,8 +1,8 @@
 // Questão 2 
 
-let ladoA = 5;
-let ladoB = 8;
-let ladoC = 10;
+let ladoA = parseInt(prompt(`Digite um número inteiro para o lado A: `));
+let ladoB = parseInt(prompt(`Digite um número inteiro para o lado B: `));
+let ladoC =  parseInt(prompt(`Digite um número inteiro para o lado C: `));
 
 let formarTriangulo = ladoA < (ladoB + ladoC) && ladoB < (ladoA + ladoC) && ladoC < (ladoA + ladoB);
 console.log(formarTriangulo);
