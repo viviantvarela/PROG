844b05bd680e687dfc518506638f507414d93f57
Vivian Varela <viviantvarela@gmail.com>
2026-08-31 11:01:11 -0300

    Alteração no nome de uma variável
    Corrected variable name from 'peso' to 'pesoIdeal' for clarity.

--- Desenvolvimento web front-end/EXERCÍCIO 2/questao4.js
+++ Desenvolvimento web front-end/EXERCÍCIO 2/questao4.js
@@ -2,7 +2,7 @@
 
 let altura = Number(prompt(`Digite sua altura em centimetros: `));
 let sexo = prompt(`Digite 'm' para masculino ou 'f' para feminino: `).toLowerCase();
-let peso;
+let pesoIdeal;
 
 if (sexo == 'm'){
   peso = (altura - 100) - ((altura - 150) / 4);
@@ -10,4 +10,4 @@ if (sexo == 'm'){
   peso = (altura - 100) - ((altura - 150) / 2);
 }
    
-console.log(`A sua altura em centímetros é ${altura}cm, seu sexo é ${sexo}, e o seu peso ideal é ${peso.toFixed(2)}kg.`)
+console.log(`A sua altura em centímetros é ${altura}cm, seu sexo é ${sexo}, e o seu peso ideal é ${pesoIdeal.toFixed(2)}kg.`)
