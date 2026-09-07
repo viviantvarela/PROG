4525ac0e7be8c9ce92de3bb475f404b6c97b4892
Vivian Varela <viviantvarela@gmail.com>
2026-09-07 14:29:41 -0300

    Questão 2 - Mudanças

--- Desenvolvimento web front-end/EXERCICIO 3/q3.ts
+++ Desenvolvimento web front-end/EXERCICIO 3/q3.ts
@@ -4,26 +4,28 @@ let inicio: number = Number(prompt('Digite o número inicial do intervalo: '));
 
 let fim: number = Number(prompt('Digite o número final do intervalo: '));
 
-if (inicio > 1 && fim >= inicio) {
+if (inicio >= 1 && fim >= inicio) {
   let contadorPrimos: number = 0;
 
 for (let num: number = inicio; num <= fim; num ++) {
+if (num > 1) {
   let primo: boolean = true;
   let divisor: number = num - 1; 
 
-while (primo && divisor > 1) {
-if (num % divisor === 0) {
-  primo = false;
-} else {
-  divisor = divisor -1;
+  while (primo && divisor > 1) {
+    if (num % divisor === 0) {
+    primo = false;
+    } else {
+    divisor = divisor -1;
   }
 }
 
 if (primo) {
   contadorPrimos++;
+    } 
   }
 }
-
+  
 console.log(`No intervalo de números de ínicio ${inicio} e fim ${fim}, existem ${contadorPrimos} números primos!`);
 
 } else {
