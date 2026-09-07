f1a1ec3f6f758376f086f405c408a07810985f30
Vivian Varela <viviantvarela@gmail.com>
2026-09-07 13:43:46 -0300

    Questão 2 - Correções
    Add logic to check if a number is prime and log the result.

--- Desenvolvimento web front-end/EXERCICIO 3/q2.ts
+++ Desenvolvimento web front-end/EXERCICIO 3/q2.ts
@@ -6,6 +6,20 @@ do {
 num = Number(prompt('Digite um número inteiro positivo (>1): '));
 } while (num <= 1); 
 
-
 let primo: boolean = true;
 let divisor: number = num - 1;
+
+
+while (primo && divisor > 1) {
+ if (num % divisor === 0) {
+   primo = false;
+ } else {
+   divisor = divisor - 1;
+ }
+}
+
+if (primo) {
+   console.log(`O número ${num} é primo.`);
+   } else {
+   console.log(`O número ${num} não é primo.`);
+   }
