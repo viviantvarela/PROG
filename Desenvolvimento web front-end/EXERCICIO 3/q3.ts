1ccfc38d896ad491866ab637db8dae578408f0cb
Vivian Varela <viviantvarela@gmail.com>
2026-09-07 14:32:11 -0300

    Questão 3 - Alterações
    Removed logic for counting prime numbers in a given range.

--- Desenvolvimento web front-end/EXERCICIO 3/q3.ts
+++ Desenvolvimento web front-end/EXERCICIO 3/q3.ts
@@ -12,22 +12,3 @@ if (num > 1) {
   let primo: boolean = true;
   let divisor: number = num - 1; 
 
-  while (primo && divisor > 1) {
-    if (num % divisor === 0) {
-    primo = false;
-    } else {
-    divisor = divisor -1;
-  }
-}
-
-if (primo) {
-  contadorPrimos++;
-    } 
-  }
-}
-  
-console.log(`No intervalo de números de ínicio ${inicio} e fim ${fim}, existem ${contadorPrimos} números primos!`);
-
-} else {
-  console.log(`O intervalo de números digitado está inválido!`);
-}
